9a8067afede973f8944a4dacc2ed07f9659747d0
Leandro Ostera <leandro@abstractmachines.dev>
2024-05-02 17:39:01 +0200

    another test without ocamlformat

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,4 @@
 let bold fmt = Spices.(default |> bold true |> build) fmt
-let () = print_endline ("Hello, " ^ (bold "Dune Developer Preview!"))
+let () = 
+  Riot.run @@ fun () ->
+    print_endline ("Hello, " ^ (bold "Dune Developer Preview!"))
